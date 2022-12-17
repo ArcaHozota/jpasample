751943d37299109e8e0864ec5891045c7c4f2c26
ArcaHozota <arkamahozota@gmail.com>
2022-12-17 12:25:18 +0900

    1.16

--- src/main/java/jp/co/toshiba/ppok/entity/City.java
+++ src/main/java/jp/co/toshiba/ppok/entity/City.java
@@ -2,48 +2,167 @@ package jp.co.toshiba.ppok.entity;
 
 import java.io.Serializable;
 
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-
-import lombok.Data;
+import javax.persistence.*;
 
 /**
  * @author Administrator
  */
-@Data
+@Entity(name = "world_city")
 public class City implements Serializable {
 
-	private static final long serialVersionUID = 1815689293387304425L;
+    private static final long serialVersionUID = 1815689293387304425L;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY.ID
-	 */
-	@TableId
-	private Long id;
+    /**
+     * This field corresponds to the database column WORLD_CITY.ID
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY.NAME
-	 */
-	private String name;
+    /**
+     * This field corresponds to the database column WORLD_CITY.NAME
+     */
+    private String name;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY.COUNTRY_CODE
-	 */
-	private String countryCode;
+    /**
+     * This field corresponds to the database column WORLD_CITY.COUNTRY_CODE
+     */
+    @Column(name = "country_code")
+    private String countryCode;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY.DISTRICT
-	 */
-	private String district;
+    /**
+     * This field corresponds to the database column WORLD_CITY.DISTRICT
+     */
+    private String district;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY.POPULATION
-	 */
-	private Long population;
+    /**
+     * This field corresponds to the database column WORLD_CITY.POPULATION
+     */
+    private Long population;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY.IS_DELETED
-	 */
-	@TableLogic
-	private Integer isDeleted;
+    /**
+     * This field corresponds to the database column WORLD_CITY.IS_DELETED
+     */
+    @Column(name = "is_deleted")
+    private Integer isDeleted;
+
+    /**
+     * getter of field id
+     *
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * setter of field id
+     *
+     * @param id WORLD_CITY.ID
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * getter of field name
+     *
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * setter of field name
+     *
+     * @param name WORLD_CITY.NAME
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * getter of field countryCode
+     *
+     * @return countryCode
+     */
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    /**
+     * setter of field countryCode
+     *
+     * @param countryCode WORLD_CITY.COUNTRY_CODE
+     */
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    /**
+     * getter of field district
+     *
+     * @return district
+     */
+    public String getDistrict() {
+        return district;
+    }
+
+    /**
+     * setter of field district
+     *
+     * @param district WORLD_CITY.DISTRICT
+     */
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    /**
+     * getter of field population
+     *
+     * @return population
+     */
+    public Long getPopulation() {
+        return population;
+    }
+
+    /**
+     * setter of field population
+     *
+     * @param population WORLD_CITY.POPULATION
+     */
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+    /**
+     * getter of field isDeleted
+     *
+     * @return isDeleted
+     */
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    /**
+     * setter of field isDeleted
+     *
+     * @param isDeleted WORLD_CITY.IS_DELETED
+     */
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                ", isDeleted=" + isDeleted +
+                '}';
+    }
 }
