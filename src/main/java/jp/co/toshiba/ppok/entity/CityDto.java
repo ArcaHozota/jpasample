f4c021775d82d3b35887f739b65b80dc1f6c9ef9
ArcaHozota <arkamahozota@gmail.com>
2022-12-17 13:07:25 +0900

    1.21

--- src/main/java/jp/co/toshiba/ppok/entity/CityDto.java
+++ src/main/java/jp/co/toshiba/ppok/entity/CityDto.java
@@ -15,38 +15,158 @@ import javax.validation.constraints.Pattern;
 @Table(name = "world_city_view")
 public class CityDto implements Serializable {
 
-	private static final long serialVersionUID = -863534569423043863L;
+    private static final long serialVersionUID = -863534569423043863L;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY_VIEW.ID
-	 */
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    /**
+     * This field corresponds to the database column WORLD_CITY_VIEW.ID
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY_VIEW.NAME
-	 */
-	@Pattern(regexp = "^[a-zA-Z_-]{4,17}$", message = "Name of cities should be in 4~17 Latin alphabets.")
-	private String name;
+    /**
+     * This field corresponds to the database column WORLD_CITY_VIEW.NAME
+     */
+    @Pattern(regexp = "^[a-zA-Z_-]{4,17}$", message = "Name of cities should be in 4~17 Latin alphabets.")
+    private String name;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY_VIEW.CONTINENT
-	 */
-	private String continent;
+    /**
+     * This field corresponds to the database column WORLD_CITY_VIEW.CONTINENT
+     */
+    private String continent;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY_VIEW.NATION
-	 */
-	private String nation;
+    /**
+     * This field corresponds to the database column WORLD_CITY_VIEW.NATION
+     */
+    private String nation;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY_VIEW.DISTRICT
-	 */
-	private String district;
+    /**
+     * This field corresponds to the database column WORLD_CITY_VIEW.DISTRICT
+     */
+    private String district;
 
-	/**
-	 * This field corresponds to the database column WORLD_CITY_VIEW.POPULATION
-	 */
-	private Long population;
+    /**
+     * This field corresponds to the database column WORLD_CITY_VIEW.POPULATION
+     */
+    private Long population;
+
+    /**
+     * getter of field id
+     *
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * setter of field id
+     *
+     * @param id WORLD_CITY_VIEW.ID
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * getter of field name
+     *
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * setter of field name
+     *
+     * @param name WORLD_CITY_VIEW.NAME
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * getter of field continent
+     *
+     * @return continent
+     */
+    public String getContinent() {
+        return continent;
+    }
+
+    /**
+     * setter of field continent
+     *
+     * @param continent WORLD_CITY_VIEW.CONTINENT
+     */
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    /**
+     * getter of field nation
+     *
+     * @return nation
+     */
+    public String getNation() {
+        return nation;
+    }
+
+    /**
+     * setter of field nation
+     *
+     * @param nation WORLD_CITY_VIEW.NATION
+     */
+    public void setNation(String nation) {
+        this.nation = nation;
+    }
+
+    /**
+     * getter of field district
+     *
+     * @return district
+     */
+    public String getDistrict() {
+        return district;
+    }
+
+    /**
+     * setter of field nation
+     *
+     * @param district WORLD_CITY_VIEW.DISTRICT
+     */
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    /**
+     * getter of field population
+     *
+     * @return population
+     */
+    public Long getPopulation() {
+        return population;
+    }
+
+    /**
+     * setter of field population
+     *
+     * @param population WORLD_CITY_VIEW.POPULATION
+     */
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "CityDto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", continent='" + continent + '\'' +
+                ", nation='" + nation + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                '}';
+    }
 }
--- src/main/java/jp/co/toshiba/ppok/entity/Nation.java
+++ src/main/java/jp/co/toshiba/ppok/entity/Nation.java
@@ -102,4 +102,314 @@ public class Nation implements Serializable {
      */
     @Column(name = "is_deleted")
     private Integer isDeleted;
+
+    /**
+     * getter of field code
+     *
+     * @return code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * setter of field code
+     *
+     * @param code WORLD_COUNTRY.CODE
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * getter of field name
+     *
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * setter of field name
+     *
+     * @param name WORLD_COUNTRY.NAME
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * getter of field continent
+     *
+     * @return continent
+     */
+    public String getContinent() {
+        return continent;
+    }
+
+    /**
+     * setter of field continent
+     *
+     * @param continent WORLD_COUNTRY.CONTINENT
+     */
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    /**
+     * getter of field region
+     *
+     * @return region
+     */
+    public String getRegion() {
+        return region;
+    }
+
+    /**
+     * setter of field region
+     *
+     * @param region WORLD_COUNTRY.REGION
+     */
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    /**
+     * getter of field surfaceArea
+     *
+     * @return surfaceArea
+     */
+    public BigDecimal getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    /**
+     * setter of field surfaceArea
+     *
+     * @param surfaceArea WORLD_COUNTRY.SURFACE_AREA
+     */
+    public void setSurfaceArea(BigDecimal surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    /**
+     * getter of field independenceYear
+     *
+     * @return independenceYear
+     */
+    public Integer getIndependenceYear() {
+        return independenceYear;
+    }
+
+    /**
+     * setter of field code
+     *
+     * @param independenceYear WORLD_COUNTRY.INDEPENDENCE_YEAR
+     */
+    public void setIndependenceYear(Integer independenceYear) {
+        this.independenceYear = independenceYear;
+    }
+
+    /**
+     * getter of field population
+     *
+     * @return population
+     */
+    public Long getPopulation() {
+        return population;
+    }
+
+    /**
+     * setter of field population
+     *
+     * @param population WORLD_COUNTRY.POPULATION
+     */
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+    /**
+     * getter of field lifeExpectancy
+     *
+     * @return lifeExpectancy
+     */
+    public BigDecimal getLifeExpectancy() {
+        return lifeExpectancy;
+    }
+
+    /**
+     * setter of field lifeExpectancy
+     *
+     * @param lifeExpectancy WORLD_COUNTRY.LIFE_EXPECTANCY
+     */
+    public void setLifeExpectancy(BigDecimal lifeExpectancy) {
+        this.lifeExpectancy = lifeExpectancy;
+    }
+
+    /**
+     * getter of field gnp
+     *
+     * @return gnp
+     */
+    public BigDecimal getGnp() {
+        return gnp;
+    }
+
+    /**
+     * setter of field gnp
+     *
+     * @param gnp WORLD_COUNTRY.GNP
+     */
+    public void setGnp(BigDecimal gnp) {
+        this.gnp = gnp;
+    }
+
+    /**
+     * getter of field gnpOld
+     *
+     * @return gnpOld
+     */
+    public BigDecimal getGnpOld() {
+        return gnpOld;
+    }
+
+    /**
+     * setter of field gnpOld
+     *
+     * @param gnpOld WORLD_COUNTRY.GNP_OLD
+     */
+    public void setGnpOld(BigDecimal gnpOld) {
+        this.gnpOld = gnpOld;
+    }
+
+    /**
+     * getter of field localName
+     *
+     * @return localName
+     */
+    public String getLocalName() {
+        return localName;
+    }
+
+    /**
+     * setter of field localName
+     *
+     * @param localName WORLD_COUNTRY.LOCAL_NAME
+     */
+    public void setLocalName(String localName) {
+        this.localName = localName;
+    }
+
+    /**
+     * getter of field governmentForm
+     *
+     * @return governmentForm
+     */
+    public String getGovernmentForm() {
+        return governmentForm;
+    }
+
+    /**
+     * setter of field governmentForm
+     *
+     * @param governmentForm WORLD_COUNTRY.GOVERNMENT_FORM
+     */
+    public void setGovernmentForm(String governmentForm) {
+        this.governmentForm = governmentForm;
+    }
+
+    /**
+     * getter of field headOfState
+     *
+     * @return headOfState
+     */
+    public String getHeadOfState() {
+        return headOfState;
+    }
+
+    /**
+     * setter of field headOfState
+     *
+     * @param headOfState WORLD_COUNTRY.HEAD_OF_STATE
+     */
+    public void setHeadOfState(String headOfState) {
+        this.headOfState = headOfState;
+    }
+
+    /**
+     * getter of field capital
+     *
+     * @return capital
+     */
+    public Long getCapital() {
+        return capital;
+    }
+
+    /**
+     * setter of field capital
+     *
+     * @param capital WORLD_COUNTRY.CAPITAL
+     */
+    public void setCapital(Long capital) {
+        this.capital = capital;
+    }
+
+    /**
+     * getter of field code2
+     *
+     * @return code2
+     */
+    public String getCode2() {
+        return code2;
+    }
+
+    /**
+     * setter of field code2
+     *
+     * @param code2 WORLD_COUNTRY.CODE2
+     */
+    public void setCode2(String code2) {
+        this.code2 = code2;
+    }
+
+    /**
+     * getter of field isDeleted
+     *
+     * @return isDeleted
+     */
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    /**
+     * setter of field isDeleted
+     *
+     * @param isDeleted WORLD_COUNTRY.IS_DELETED
+     */
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    @Override
+    public String toString() {
+        return "Nation{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", continent='" + continent + '\'' +
+                ", region='" + region + '\'' +
+                ", surfaceArea=" + surfaceArea +
+                ", independenceYear=" + independenceYear +
+                ", population=" + population +
+                ", lifeExpectancy=" + lifeExpectancy +
+                ", gnp=" + gnp +
+                ", gnpOld=" + gnpOld +
+                ", localName='" + localName + '\'' +
+                ", governmentForm='" + governmentForm + '\'' +
+                ", headOfState='" + headOfState + '\'' +
+                ", capital=" + capital +
+                ", code2='" + code2 + '\'' +
+                ", isDeleted=" + isDeleted +
+                '}';
+    }
 }
