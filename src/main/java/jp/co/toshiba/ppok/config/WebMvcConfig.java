ab3ad4ffcfe0de8bfb78a3f61437cb526f8cdfd6
ArcaHozota <arkamahozota@gmail.com>
2022-12-18 11:24:06 +0900

    1.42

--- src/main/java/jp/co/toshiba/ppok/config/WebMvcConfig.java
+++ src/main/java/jp/co/toshiba/ppok/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package jp.co.toshiba.ppok.config;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -8,8 +9,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import jp.co.toshiba.ppok.utils.JacksonObjectMapper;
-
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Administrator
@@ -18,32 +18,31 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
-    /**
-     * SpringMVCフレームワークを拡張するメッセージ・コンバーター
-     *
-     * @param converters コンバーター
-     */
-    @Override
-    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        log.info("拡張メッセージコンバーターの設置は完了しました。");
-        // 創建消息轉換器對象；
-        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
-        // 設置對象轉換器，底層使用Jackson將Java對象轉為JSON；
-        messageConverter.setObjectMapper(new JacksonObjectMapper());
-        // 將上述消息轉換器追加到SpringMVC框架的轉換器容器中；
-        converters.add(0, messageConverter);
-    }
+	/**
+	 * SpringMVCフレームワークを拡張するメッセージ・コンバーター
+	 *
+	 * @param converters コンバーター
+	 */
+	@Override
+	protected void extendMessageConverters(final List<HttpMessageConverter<?>> converters) {
+		log.info("拡張メッセージコンバーターの設置は完了しました。");
+		// 創建消息轉換器對象；
+		final MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+		// 設置對象轉換器，底層使用Jackson將Java對象轉為JSON；
+		messageConverter.setObjectMapper(new JacksonObjectMapper());
+		// 將上述消息轉換器追加到SpringMVC框架的轉換器容器中；
+		converters.add(0, messageConverter);
+	}
 
-    /**
-     * 設置靜態資源映射
-     *
-     * @param registry 注冊説明
-     */
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        log.info("静的リソースのマッピングが開始しました。");
-        registry.addResourceHandler("/jquery/**").addResourceLocations("classpath:/static/jquery/");
-        registry.addResourceHandler("/bootstrap/**").addResourceLocations("classpath:/static/bootstrap/");
-        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
-    }
+	/**
+	 * 設置靜態資源映射
+	 *
+	 * @param registry 注冊説明
+	 */
+	@Override
+	protected void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		log.info("静的リソースのマッピングが開始しました。");
+		registry.addResourceHandler("/jquery/**").addResourceLocations("classpath:/static/jquery/");
+		registry.addResourceHandler("/bootstrap/**").addResourceLocations("classpath:/static/bootstrap/");
+	}
 }
