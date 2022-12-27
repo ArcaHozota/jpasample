9411370b556b1de0d6bd4355ee7752cb6b990885
ArcaHozota <arkamahozota@gmail.com>
2022-12-27 12:14:30 +0900

    2.17

---
+++ src/main/java/jp/co/sony/ppog/bean/CityView.java
@@ -0,0 +1,49 @@
+package jp.co.sony.ppog.bean;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * dto of the view of world cities
+ *
+ * @author Administrator
+ */
+
+@Data
+public class CityView implements Serializable {
+
+	private static final long serialVersionUID = 6678964783710878220L;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.ID
+	 */
+	private Long id;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.NAME
+	 */
+	@Pattern(regexp = "^[a-zA-Z_-]{4,17}$", message = "Name of cities should be in 4~17 Latin alphabets.")
+	private String name;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.CONTINENT
+	 */
+	private String continent;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.NATION
+	 */
+	private String nation;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.DISTRICT
+	 */
+	private String district;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.POPULATION
+	 */
+	private Long population;
+}
---
+++ src/main/java/jp/co/sony/ppog/config/MyBatisPlusConfig.java
@@ -0,0 +1,31 @@
+package jp.co.sony.ppog.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatisPlus Pagination
+ *
+ * @author Administrator
+ */
+@Configuration
+public class MyBatisPlusConfig {
+
+	/**
+	 * configuration for MBPlus intercepter
+	 *
+	 * @return mybatisPlusInterceptor
+	 */
+	@Bean
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		final MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+		final PaginationInnerInterceptor innerInterceptor = new PaginationInnerInterceptor();
+		innerInterceptor.setDbType(DbType.ORACLE_12C);
+		innerInterceptor.setOverflow(true);
+		mybatisPlusInterceptor.addInnerInterceptor(innerInterceptor);
+		return mybatisPlusInterceptor;
+	}
+}
