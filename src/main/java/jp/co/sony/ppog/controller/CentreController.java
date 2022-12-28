d2c88ded77b06e8de1c4e6502d54b4e76a076d6c
ArcaHozota <arkamahozota@gmail.com>
2022-12-28 21:51:52 +0900

    2.48

--- src/main/java/jp/co/sony/ppog/controller/CentreController.java
+++ src/main/java/jp/co/sony/ppog/controller/CentreController.java
@@ -7,7 +7,6 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.google.common.collect.Lists;
 import jp.co.sony.ppog.entity.CityView;
-import jp.co.sony.ppog.entity.Country;
 import jp.co.sony.ppog.service.CityViewService;
 import jp.co.sony.ppog.utils.RestMsg;
 import org.apache.commons.lang3.StringUtils;
@@ -37,15 +36,15 @@ public class CentreController {
     @GetMapping(value = "/city")
     public ModelAndView getCityInfo(@RequestParam(value = "pageNum", defaultValue = "1") final Integer pageNum,
                                     @RequestParam(value = "keyword", defaultValue = "") final String keyword) {
-        // 聲明分頁構造器；
+        // ページングコンストラクタを宣言する；
         final Page<CityView> pageInfo = Page.of(pageNum, 17);
-        // 聲明條件構造器；
+        // 検索条件コンストラクタを宣言する；
         final LambdaQueryWrapper<CityView> queryWrapper = Wrappers.lambdaQuery(new CityView());
-        // 添加過濾條件；
+        // フィルター条件を設定する；
         queryWrapper.like(StringUtils.isNotEmpty(keyword), CityView::getName, keyword);
-        // 添加排序條件；
+        // ソート条件を設定する；
         queryWrapper.orderByAsc(CityView::getId);
-        // 執行查詢；
+        // ページング検索；
         this.cityViewService.page(pageInfo, queryWrapper);
         // modelAndViewオブジェクトを宣言する；
         final ModelAndView mav = new ModelAndView("index");
@@ -69,6 +68,11 @@ public class CentreController {
         return mav;
     }
 
+    /**
+     * 指定された都市の情報を取得する
+     * @param id　都市ID
+     * @return 都市情報
+     */
     @GetMapping(value = "/city/{id}")
     @ResponseBody
     public RestMsg getCityInfo(@PathVariable("id") final Long id) {
