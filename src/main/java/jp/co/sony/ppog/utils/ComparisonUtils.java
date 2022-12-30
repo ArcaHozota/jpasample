7827a6aa258f7628bb13b415efb7514cb38e2b98
ArcaHozota <arkamahozota@gmail.com>
2022-12-30 16:33:01 +0900

    3.03

---
+++ src/main/java/jp/co/sony/ppog/utils/ComparisonUtils.java
@@ -0,0 +1,40 @@
+package jp.co.sony.ppog.utils;
+
+/**
+ * 判断ツールクラス
+ *
+ * @author Administrator
+ * @date 2022-11-07
+ */
+public class ComparisonUtils {
+
+	/**
+	 * イコール
+	 * 
+	 * @param ob1 値
+	 * @param ob2 値
+	 * @return 結果
+	 */
+	public static boolean isEqual(final Object ob1, final Object ob2) {
+		boolean isEqual = false;
+		if (ob1 != null && ob2 != null && ob1.getClass().equals(ob2.getClass())) {
+			final String strOb1 = ob1.toString();
+			final String strOb2 = ob2.toString();
+			isEqual = strOb1.equals(strOb2);
+		} else if (ob1 == null && ob2 == null) {
+			isEqual = true;
+		}
+		return isEqual;
+	}
+
+	/**
+	 * 不イコール
+	 * 
+	 * @param ob1 値
+	 * @param ob2 値
+	 * @return 結果
+	 */
+	public static boolean isNotEqual(final Object ob1, final Object ob2) {
+		return !isEqual(ob1, ob2);
+	}
+}
