44c195e16bf98841ff0857baf8e9cb80732e4c3c
ArcaHozota <arkamahozota@gmail.com>
2023-01-02 17:13:14 +0900

    3.29

--- src/main/java/jp/co/sony/ppog/mapper/CityViewMapper.java
+++ src/main/java/jp/co/sony/ppog/mapper/CityViewMapper.java
@@ -1,7 +1,5 @@
 package jp.co.sony.ppog.mapper;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
@@ -13,19 +11,4 @@ import jp.co.sony.ppog.entity.CityView;
  */
 @Mapper
 public interface CityViewMapper extends BaseMapper<CityView> {
-
-	/**
-	 * Search continents of cities located on.
-	 *
-	 * @return List<CityDto>
-	 */
-	List<String> selectContinents();
-
-	/**
-	 * Search nation's name of cities.
-	 *
-	 * @param continent name of continent which the nation located on.
-	 * @return List<CityDto>
-	 */
-	List<String> selectNations(String continent);
 }
--- src/main/java/jp/co/sony/ppog/mapper/CountryMapper.java
+++ src/main/java/jp/co/sony/ppog/mapper/CountryMapper.java
@@ -1,9 +1,28 @@
 package jp.co.sony.ppog.mapper;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import jp.co.sony.ppog.entity.Country;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import jp.co.sony.ppog.entity.Country;
 
 @Mapper
 public interface CountryMapper extends BaseMapper<Country> {
+
+	/**
+	 * Search continents of cities located on.
+	 *
+	 * @return List<CityDto>
+	 */
+	List<String> selectContinents();
+
+	/**
+	 * Search nation's name of cities.
+	 *
+	 * @param continent name of continent which the nation located on.
+	 * @return List<CityDto>
+	 */
+	List<String> selectNations(String continent);
 }
