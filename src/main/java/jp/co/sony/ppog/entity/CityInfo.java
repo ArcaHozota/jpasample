e4f95a9e33a04b3e6cc6b726cc549aefd072a539
ArcaHozota <arkamahozota@gmail.com>
2023-02-04 23:59:50 +0900

    3.60

--- src/main/java/jp/co/sony/ppog/entity/CityInfo.java
+++ src/main/java/jp/co/sony/ppog/entity/CityInfo.java
@@ -27,7 +27,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @Proxy(lazy = false)
-@Table(name = "city_mate_view")
+@Table(name = "city_mateview")
 @NamedQuery(name = "CityInfo.findByNations", query = "select c from CityInfo c where c.nation = :nation")
 @NamedQuery(name = "CityInfo.getByNations", query = "select c from CityInfo c where c.nation = :nation order by c.id asc")
 @NamedQuery(name = "CityInfo.getByNames", query = "select c from CityInfo c where c.name like concat('%', :name, '%') order by c.id asc")
