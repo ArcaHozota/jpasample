24f180743536b4714a3d414f64f807b105415aa3
ArcaHozota <arkamahozota@gmail.com>
2023-02-05 12:35:03 +0900

    3.63

--- src/main/java/jp/co/sony/ppog/mapper/CityDao.java
+++ src/main/java/jp/co/sony/ppog/mapper/CityDao.java
@@ -1,12 +1,12 @@
 package jp.co.sony.ppog.mapper;
 
-import org.postgresql.util.PSQLException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.sony.ppog.entity.City;
+import oracle.jdbc.driver.OracleSQLException;
 
 public interface CityDao extends JpaRepository<City, Integer> {
 
@@ -16,6 +16,6 @@ public interface CityDao extends JpaRepository<City, Integer> {
 	 * @param id id of the selected city
 	 */
 	@Modifying
-	@Transactional(rollbackFor = PSQLException.class)
+	@Transactional(rollbackFor = OracleSQLException.class)
 	void removeById(@Param("id") final Integer id);
 }
