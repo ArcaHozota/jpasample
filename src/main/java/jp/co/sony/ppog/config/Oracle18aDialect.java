4ac5522bafe04496952be4031a5aa9f29b67f60e
ArcaHozota <arkamahozota@gmail.com>
2023-02-06 13:54:49 +0900

    3.90

--- src/main/java/jp/co/sony/ppog/config/Oracle18aDialect.java
+++ src/main/java/jp/co/sony/ppog/config/Oracle18aDialect.java
@@ -5,6 +5,7 @@ import java.sql.Types;
 import org.hibernate.dialect.Oracle12cDialect;
 import org.hibernate.type.StandardBasicTypes;
 
+@SuppressWarnings("unused")
 public class Oracle18aDialect extends Oracle12cDialect {
 
 	public Oracle18aDialect() {
