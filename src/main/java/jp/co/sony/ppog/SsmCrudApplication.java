47ac14890c6bb18b5779b0524bd91a15d9dc76d5
ArcaHozota <arkamahozota@gmail.com>
2023-04-11 01:08:45 +0900

    4.85

--- src/main/java/jp/co/sony/ppog/SsmCrudApplication.java
+++ src/main/java/jp/co/sony/ppog/SsmCrudApplication.java
@@ -1,10 +1,10 @@
 package jp.co.sony.ppog;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * ssmcrudアプリケーション
@@ -12,10 +12,12 @@ import lombok.extern.slf4j.Slf4j;
  * @author Administrator
  * @date 2022-12-12
  */
-@Slf4j
 @SpringBootApplication
 @ServletComponentScan
 public class SsmCrudApplication {
+
+	private static final Logger log = LogManager.getLogger(SsmCrudApplication.class);
+
 	public static void main(final String[] args) {
 		SpringApplication.run(SsmCrudApplication.class, args);
 		log.info("アプリは正常に起動しました!");
