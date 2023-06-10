76623a1b53b6a7fe1692f5b440ee2987e9ed9921
ArcaHozota <arkamahozota@gmail.com>
2023-06-10 23:55:28 +0900

    4.93

--- src/main/java/jp/co/sony/ppog/config/Oracle18cDialect.java
+++ src/main/java/jp/co/sony/ppog/config/Oracle18cDialect.java
@@ -5,9 +5,9 @@ import java.sql.Types;
 import org.hibernate.dialect.Oracle12cDialect;
 import org.hibernate.type.StandardBasicTypes;
 
-public class Oracle18aDialect extends Oracle12cDialect {
+public class Oracle18cDialect extends Oracle12cDialect {
 
-	public Oracle18aDialect() {
+	public Oracle18cDialect() {
 		super();
 		this.registerHibernateType(Types.NCHAR, StandardBasicTypes.STRING.getName());
 	}
