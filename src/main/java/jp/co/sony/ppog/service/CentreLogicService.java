eb25cc25f408499a04c91f7310797d1d4d494d1f
Arca Hozota <arkamahozota@gmail.com>
2023-09-28 16:19:53 +0900

    6.97

--- src/main/java/jp/co/sony/ppog/service/CentreLogicService.java
+++ src/main/java/jp/co/sony/ppog/service/CentreLogicService.java
@@ -30,7 +30,7 @@ public interface CentreLogicService {
 	 * @param id 都市ID
 	 * @return CityInfoDto
 	 */
-	CityDto getCityInfoById(Long id);
+	CityDto getCityInfoById(Integer id);
 
 	/**
 	 * 都市IDによって国家名を抽出する
@@ -38,7 +38,7 @@ public interface CentreLogicService {
 	 * @param id 都市ID
 	 * @return List<String>
 	 */
-	List<String> getListOfNationsById(Long id);
+	List<String> getListOfNationsById(Integer id);
 
 	/**
 	 * 入力した都市情報を保存する
@@ -59,7 +59,7 @@ public interface CentreLogicService {
 	 *
 	 * @param id 都市ID
 	 */
-	void removeById(Long id);
+	void removeById(Integer id);
 
 	/**
 	 * 大陸情報を取得する
