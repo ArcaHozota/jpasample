eba0c436ea0b15e4e72d57989f43ddec96ebb0b2
ArkamaHozota <arkamahozota@gmail.com>
2023-10-14 19:33:05 +0900

    7.17

--- src/main/java/jp/co/sony/ppog/controller/CentreController.java
+++ src/main/java/jp/co/sony/ppog/controller/CentreController.java
@@ -3,7 +3,6 @@ package jp.co.sony.ppog.controller;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import jp.co.sony.ppog.dto.CityDto;
 import jp.co.sony.ppog.entity.City;
@@ -29,8 +28,7 @@ import lombok.RequiredArgsConstructor;
  * @author ArcaHozota
  * @since 1.00beta
  */
-@Controller
-@ResponseBody
+@RestController
 @RequestMapping("/ssmcrud")
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class CentreController {
