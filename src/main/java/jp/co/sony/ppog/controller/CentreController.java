ae5b96bead244cad14fcd8d69ce6d8bfe376a864
ArkamaHozota <arkamahozota@gmail.com>
2023-10-14 19:32:45 +0900

    7.16

--- src/main/java/jp/co/sony/ppog/controller/CentreController.java
+++ src/main/java/jp/co/sony/ppog/controller/CentreController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import jp.co.sony.ppog.dto.CityDto;
 import jp.co.sony.ppog.entity.City;
@@ -31,6 +30,7 @@ import lombok.RequiredArgsConstructor;
  * @since 1.00beta
  */
 @Controller
+@ResponseBody
 @RequestMapping("/ssmcrud")
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class CentreController {
@@ -47,7 +47,6 @@ public class CentreController {
 	 * @return 処理成功のメッセージ
 	 */
 	@GetMapping(value = "/check")
-	@ResponseBody
 	public RestMsg checkName(@RequestParam("cityName") final String cityName) {
 		if (!cityName.matches(Messages.MSG006)) {
 			return RestMsg.failure().add("validatedMsg", Messages.MSG005);
@@ -66,7 +65,6 @@ public class CentreController {
 	 * @return 処理成功のメッセージ
 	 */
 	@DeleteMapping(value = "/city/{id}")
-	@ResponseBody
 	public RestMsg deleteCityInfo(@PathVariable("id") final Integer id) {
 		this.centreLogicService.removeById(id);
 		return RestMsg.success();
@@ -79,7 +77,6 @@ public class CentreController {
 	 * @return 都市情報
 	 */
 	@GetMapping(value = "/city/{id}")
-	@ResponseBody
 	public RestMsg getCityInfo(@PathVariable("id") final Integer id) {
 		final CityDto cityDto = this.centreLogicService.getCityInfoById(id);
 		return RestMsg.success().add("citySelected", cityDto);
@@ -95,25 +92,6 @@ public class CentreController {
 			@RequestParam(value = "keyword", defaultValue = StringUtils.EMPTY_STRING) final String keyword) {
 		// ページング検索結果を吹き出します；
 		final Page<CityDto> pageInfo = this.centreLogicService.getPageInfo(pageNum, keyword);
-		// modelAndViewオブジェクトを宣言する；
-		final ModelAndView modelAndView = new ModelAndView("index");
-		// 前のページを取得する；
-		final int current = pageInfo.getNumber();
-		// ページングナビゲーションの数を定義する；
-		final int naviNums = 7;
-		// ページングナビの最初と最後の数を取得する；
-		final int pageFirstIndex = (current / naviNums) * naviNums;
-		int pageLastIndex = (((current / naviNums) + 1) * naviNums) - 1;
-		if (pageLastIndex > (pageInfo.getTotalPages() - 1)) {
-			pageLastIndex = pageInfo.getTotalPages() - 1;
-		} else {
-			pageLastIndex = (((current / naviNums) + 1) * naviNums) - 1;
-		}
-		modelAndView.addObject("pageInfo", pageInfo);
-		modelAndView.addObject("keyword", keyword);
-		modelAndView.addObject("pageFirstIndex", pageFirstIndex);
-		modelAndView.addObject("pageLastIndex", pageLastIndex);
-		modelAndView.addObject("title", "CityList");
 		return RestMsg.success().add("pageInfo", pageInfo);
 	}
 
@@ -123,7 +101,6 @@ public class CentreController {
 	 * @return 大陸名称のリスト
 	 */
 	@GetMapping(value = "/continents")
-	@ResponseBody
 	public RestMsg getContinents() {
 		final List<String> continents = this.centreLogicService.findAllContinents();
 		return RestMsg.success().add("continentList", continents);
@@ -136,7 +113,6 @@ public class CentreController {
 	 * @return 言語のリスト
 	 */
 	@GetMapping(value = "/languages")
-	@ResponseBody
 	public RestMsg getListOfLanguages(@RequestParam("nationVal") final String nationVal) {
 		final String language = this.centreLogicService.findLanguageByCty(nationVal);
 		return RestMsg.success().add("languages", language);
@@ -149,7 +125,6 @@ public class CentreController {
 	 * @return 国のリスト
 	 */
 	@GetMapping(value = "/nations/{id}")
-	@ResponseBody
 	public RestMsg getListOfNationsById(@PathVariable("id") final Integer id) {
 		final List<String> nations = this.centreLogicService.getListOfNationsById(id);
 		return RestMsg.success().add("nationsWithName", nations);
@@ -162,7 +137,6 @@ public class CentreController {
 	 * @return 国のリスト
 	 */
 	@GetMapping(value = "/nations")
-	@ResponseBody
 	public RestMsg getListOfNationsById(@RequestParam("continentVal") final String continentVal) {
 		final List<String> nations = this.centreLogicService.findNationsByCnt(continentVal);
 		return RestMsg.success().add("nationList", nations);
@@ -175,7 +149,6 @@ public class CentreController {
 	 * @return 処理成功のメッセージ
 	 */
 	@PostMapping(value = "/city")
-	@ResponseBody
 	public RestMsg saveCityInfo(@RequestBody final CityDto cityDto) {
 		this.centreLogicService.save(cityDto);
 		return RestMsg.success();
@@ -188,7 +161,6 @@ public class CentreController {
 	 * @return 処理成功のメッセージ
 	 */
 	@PutMapping(value = "/city/{id}")
-	@ResponseBody
 	public RestMsg updateCityInfo(@RequestBody final CityDto cityDto) {
 		this.centreLogicService.update(cityDto);
 		return RestMsg.success();
